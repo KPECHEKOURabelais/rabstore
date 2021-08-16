ea01916c8cdc8fd6326b322489cca52b7e8ef7bd
Rabelais KPECHEKOU <kpechekou.arminius@gmail.com>
2021-08-16 23:02:12 +0100

    Nouveau fichier
    Accueil et header

---
+++ accueil.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Accueil </title>
+    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-dark fixed-top bg-dark ">
+      <img src="TIC.gif" alt="" height="130px">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01"
+        aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon " id="#haut"></span>
+      </button>
+
+      <div class="text-right collapse navbar-collapse" id="navbarsExample01">
+        <ul class="navbar-nav mr-auto  ">
+
+          <li class="nav-item">
+            <a class="transition" class="nav-link" href="index.html" >Accueil</a>
+          </li>
+
+
+          <li class="nav-item">
+            <a class="transition" class="nav-link" href="mention.html#a_propos" >A propos</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="transition" class="nav-link" href="plan_du_site.html" >Plan du Site</a>
+          </li>
+
+
+          <li class="nav-item">
+            <a class="transition" class="nav-link" href="#contacts">Contacts</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </nav>
+  </header>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script>
+    window.jQuery || document.write('<script src="jquery-slim.min.js"><\/script>')
+  </script>
+  <script src="popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
