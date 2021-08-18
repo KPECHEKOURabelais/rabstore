50099966fd1041f126f5ab1850db72bd18a4b22f
Rabelais KPECHEKOU <kpechekou.arminius@gmail.com>
2021-08-18 02:03:14 +0100

    modification

--- accueil.php
+++ accueil.php
@@ -13,11 +13,30 @@
         <section id="accueil">
             <article>
             <h2> Rab Store, la bibliothèque des mathématiques et de la littérature </h2>
-            <div class="row">
-                <div class="col-lg-4">
-                   <img src="voltaire.jpg" alt=" voltaire">
+                <div class="baniere-image">
+                    <img src="etudiants1.jpg" alt="Des étudiants" onclick="this.src='etudiants2.jpeg'">
                 </div>
-                <div class="col-lg-8">
+
+                <div class="discours">
+                    <p >
+                        L'<span class="education">Education</span> a toujours été l'un des piliers de la rigidité de notre société.
+                        Du latin educatio, l'éducation désigne l'action d'élever , de former un enfant, un jeune homme, une jeune fille de développer
+                        ses facultés intéllectuelles et morales. Ainsi, ce site vient pour aider ses visiteurs dans leur formation universitaire, leur étude scolaire et tout autre formation.
+                        <span class="rab"> Rab Store </span> vous fera voir comprendre des éléments nécessaires pour votre épanouissement intellectuelle.  
+                        <br>
+                        Et même, la littérature et les mathématiques ont depuis toujours cohabité pour conduire le monde vers sa réussite.
+                    Les grands matheux que vous voyez peu après ne vous feront pas comprendre le contraire. Et bien, débutons par ce cadre afin que vous découvriez 
+                    <a href="plan_du_site.php" style=" color: red; "> les bienfaits de ce site </a> 
+                    </p>
+                    <p>
+                        Je vous laisse de vous même découvrir les contenus du site, dont l'accès a été rendu possible seulement pour les étudiants en IG 1 à l'ENEAM.
+                    </p>
+                    
+                </div>
+                <h3> Mathématiciens , Littéraires sont-ils ? Découvrez-les !</h3>
+            <div class="auteur">
+                <img src="voltaire.jpg" alt=" voltaire">
+                <div class="description">
                         <p>
                         Célèbre écrivain, poète et philosophe français, François Marie Arouet, dit Voltaire, est né à Paris le 21 novembre 1694 et meurt le 30 mai 1978.
                         </p>
@@ -28,36 +47,34 @@
                         </div>
                 </div>    
             </div>
-            <div class="auteur">
+            <div class="auteur1">
                     <img src="lagrange.png" alt="Lagrange">
                     <div class="description1">
                         <p>
                             Né en 1736 et mort à Paris en 1813, Joseph Louis de Lagrange fut un mathématicien, mécanicien etc. 
                         </p>
-                        <div class="oeuvre">
+                        <div class="oeuvre1">
                         <h3>  Son oeuvre  </h3>
                         <p>
                             Il est le fondateur du calcul des variations, avec Euler, et de la théorie des formes quadratiques. <br>
-                            En physique, en précisant le principe de moindre action, avec le calcul des variations, vers 1756, il invente la fonction de Lagrange, qui vérifie les équations de Lagrange, puuis développe la mécanique analylitique, vers 1788.
-                            Il a bien aussi mené des recherches dans le domaine de l'astronomie. 
+                            En physique, en précisant le principe de moindre action, avec le calcul des variations, vers 1756, il invente la fonction de Lagrange, qui vérifie les équations de Lagrange.
                         </p>
                         </div>
                     </div>  
                     
             </div>
-            <div class="auteur">
+            <div class="auteur2">
                     <img src="Blaise-Pascal.jpg" alt="Blaise-Pascal">
                     
-                    <div class="description1">
+                    <div class="description2">
                         <p>
                             Né le 19 juin 1623 à Clermont et mort le 19 août 1662 à Paris, Blaise Pascal est un mathématicien, 
                             physicien, philosophe, moraliste et théologien français. 
                         </p>
-                        <div class="oeuvre">
+                        <div class="oeuvre2">
                         <h3> Oeuvre et Citation   </h3>
                         <p>
-                            A l'âge de 19 ans, il invente la première machine à calculer et après trois ans de développpement et une cinquantaine de prototypes. 
-                            Il la présente à ses contemporains en la dédiant au chancelier Séguier.
+                            A l'âge de 19 ans, il invente la première machine à calculer.
                             <br>
                             <strong> Citation populaire : </strong><br>
                             << Le coeur a ses raisons que la raison elle-même ignore. >>
--- algorithme.php
+++ algorithme.php
@@ -5,8 +5,74 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Algorithme </title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <?php require_once('header.html') ?>
+    <section>
+        <article class="algo">
+            <div class="Exo">
+            <h1> Initiation à l’algorithmique </h1>
+            <h3> Exercice 1 : </h3>
+            <p>
+            Ecrire un algorithme qui permet d’afficher le résultat d’un étudiant (accepté ou rejeté) à un module, sachant que ce module est évalué par une note d’oral de coefficient 1, et une note d’écrit de coefficient 2. La moyenne obtenue doit-être supérieure ou égale à 10 pour valider le module. Comme entrées, on a la note d’oral et la note d’écrit, ensuite on calcule la moyenne, et on affiche le résultat.
+            </p>
+            <span class="puce"> ► </span> <a href="#exo1" style="color : #deffea; text-decoration : underline;"> Corrigé Exo 1 </a>
+            <pre>
+            
+            </pre>  
+            <p>
+                Le corrigé de cet exercice se trouve ci-joint un peu après à l'énoncé. Découvrons d'abord les énoncés de ces exercices de l'examen de la session de Mai de l'ENEAM IG 1.
+            </p>
+            <h3> Exercice 2 : </h3>
+            <p>
+			Ecrire un algorithme qui calcule et affiche le ticket de caisse de la façon suivante : 
+            <ul>
+                <li> Saisir le prix HT du produit </li>
+                <li> Saisir la quantité </li>
+                <li> Et ceci jusqu’à ce que la caissière termine en tapant 0 comme prix unitaire </li>
+                <li> Saisir alors le pourcentage de remise auquel le client à droit. </li>
+            </ul>
+
+            Afficher le prix total à payer en ajoutant la TVA à 18% en déduisant la remise.
+
+            </p>
+            <p>
+                <h3> Exercice 3 : </h3>
+            
+		On considère des candidats inscrits à une formation diplômante. Chaque candidat va obtenir une note pour cette formation. 
+        Ecrire l’algorithme permettant d’afficher la liste des candidats dont la note est supérieure ou égale à la moyenne des notes de
+        <span  id="exo1"> tous les <span> candidats (les noms des candidats et les notes étant lus à partir du clavier, et stockés dans des tableaux).
+
+            </p>
+            </div>
+            <div class="corrige">
+                    <div id="exo1">
+                        <h3> Corrigé 1 : </h3>
+                    </div>
+                    <div class="tab-corrige">
+                   <pre>
+                   Algorithme 			Résultat d’un étudiant ;
+		Variable 
+		note_ecrit, note_orale, moy : réel ;
+		Début 
+		Ecrireln(‘’Entrer la note d’oral de l’étudiant : ‘’) ;
+		Lire(note_oral) ;
+		Ecrireln(‘’Entrer la note d’écrit de l’ étudiant : ‘’) ;
+		Lire(note_ecrit) ;
+		moy ← ( 2*note_ecrit + note_oral ) / 3 ;
+		SI moy >= 10 ALORS 
+			Ecrireln(‘’Accepté’’) ;
+		SINON 
+			Ecrireln(‘’Rejeté’’) ;
+		Fin.
+
+                   </pre>
+
+                    </div>
+            </div>
+        </article>
+    </section>
+    <?php require_once('footer.html') ?>
 </body>
 </html>
